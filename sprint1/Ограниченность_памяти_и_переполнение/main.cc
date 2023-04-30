d123162d8a4207ae4337d33b590954ddc3d5254a
Goblin Tagganaro <AYGlazkov@stud.kpfu.ru>
2023-04-30 12:18:20 +0300

    owerflow

--- sprint1/Ограниченность_памяти_и_переполнение/main.cc
+++ sprint1/Ограниченность_памяти_и_переполнение/main.cc
@@ -5,13 +5,26 @@
 using namespace std;
 
 int main() {
-    const auto min = std::numeric_limits<int64_t>::min();
-    const auto max = std::numeric_limits<int64_t>::max();
+    {
+        const auto min = std::numeric_limits<int64_t>::min();
+        const auto max = std::numeric_limits<int64_t>::max();
 
-    std::cout << min << std::endl;
-    std::cout << max << std::endl;
-    std::cout << min + max << std::endl;
-    std::cout << min * 2 << std::endl;
-    std::cout << max * 2 << std::endl;
+        std::cout << "min<int64_t> " << min << std::endl;
+        std::cout << "max<int64_t> " << max << std::endl;
+        std::cout << "min + max<int64_t> "<< min + max << std::endl;
+        std::cout << "2 * min<int64_t> "<<min * 2 << std::endl;
+        std::cout << "2 * max<int64_t> " << max * 2 << std::endl;
+    }
+    std::cout << "_____________________________________________\n";
+    {
+        const auto min = std::numeric_limits<uint64_t>::min();
+        const auto max = std::numeric_limits<uint64_t>::max();
+
+        std::cout << "min<uint64_t> " << min << std::endl;
+        std::cout << "max<uint64_t> " << max << std::endl;
+        std::cout << "min + max<uint64_t> "<< min + max << std::endl;
+        std::cout << "2 * min<uint64_t> "<<min * 2 << std::endl;
+        std::cout << "2 * max<uint64_t> " << max * 2 << std::endl;
+    }
     return 0;
 }
