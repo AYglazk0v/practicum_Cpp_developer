9d59aaad093fa4571df67754e06043a97e156a9c
Goblin Tagganaro <AYGlazkov@stud.kpfu.ru>
2023-05-08 12:55:54 +0300

    modified:   Task1.cc, Task2.cc

--- sprint4/Итераторы/Понятие_итератора/Task1.cc
+++ sprint4/Итераторы/Понятие_итератора/Task1.cc
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <ranges>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -9,5 +10,6 @@ int main() {
     vector<string> langs = {"Python"s, "Java"s, "C#"s, "Ruby"s, "C++"s};
     // Выведите первый язык, начинающийся на C, используя алгоритм find_if
     std::cout << *std::find_if(langs.begin(), langs.end(), [](const std::string& s){return s[0] == 'C';}) << '\n';
+    std::cout << *std::ranges::find_if(langs, [](const std::string& s){return s[0] == 'C';});
     return 0;
 }
--- sprint4/Итераторы/Понятие_итератора/Task2.cc
+++ sprint4/Итераторы/Понятие_итератора/Task2.cc
@@ -1,5 +1,6 @@
+#include <algorithm>
 #include <iostream>
-#include <string>
+#include <iterator>
 #include <vector>
 
 using namespace std;
@@ -12,6 +13,8 @@ struct Lang {
 int main() {
     vector<Lang> langs = {{"Python"s, 29}, {"Java"s, 24}, {"C#"s, 20}, {"Ruby"s, 25}, {"C++"s, 37}};
     auto it = std::find_if(langs.begin(), langs.end(), [](const Lang& l){return l.name[0] == 'J';});
-    std::cout << it->name << ", "s << it->age;
+    std::cout << it->name << ", "s << it->age << '\n';
+    auto it_range = std::ranges::find_if(langs, [](const Lang& l){return l.name[0] == 'J';});
+    std::cout << it_range->name << ", "s << it_range->age << '\n';
     return 0;
 }
