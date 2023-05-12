d3926d887ce2d970a7d8f372ec5b070af9eb3afd
Goblin Tagganaro <AYGlazkov@stud.kpfu.ru>
2023-05-12 15:19:25 +0300

    deleted trash

--- sprint4/Распределение_кода_по_файлам/Делим_проект_на_файлы/Task1/carpenter.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "builder.h"
-#include "wall.h"
-
-class Carpenter {
-public:
-    int CalcShelves(const Wall& wall) const;
-    void InstallDoor(Wall& wall, const Builder& builder) const;
-};
--- sprint4/Распределение_кода_по_файлам/Делим_проект_на_файлы/Task1/square_calculation.cpp
+++
@@ -1,5 +0,0 @@
-#include "square_calculation.h"
-
-double CalcSquare(double width, double height) {
-    return width * height;
-}
