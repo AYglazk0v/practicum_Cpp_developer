4891ed8f13de979f1c89171f2b73ac708a18069e
Goblin Tagganaro <AYGlazkov@stud.kpfu.ru>
2023-05-22 17:53:26 +0300

    Need testing in Linux

---
+++ sprint8/Параллельные_алгоритмы/Параллелим_методы_поисковой_системы/Task/build/CMakeFiles/search_server.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/search_server.dir/document.cpp.o"
+  "CMakeFiles/search_server.dir/document.cpp.o.d"
+  "CMakeFiles/search_server.dir/main.cpp.o"
+  "CMakeFiles/search_server.dir/main.cpp.o.d"
+  "CMakeFiles/search_server.dir/process_queries.cpp.o"
+  "CMakeFiles/search_server.dir/process_queries.cpp.o.d"
+  "CMakeFiles/search_server.dir/profiling.cpp.o"
+  "CMakeFiles/search_server.dir/profiling.cpp.o.d"
+  "CMakeFiles/search_server.dir/read_input_functions.cpp.o"
+  "CMakeFiles/search_server.dir/read_input_functions.cpp.o.d"
+  "CMakeFiles/search_server.dir/remove_duplicates.cpp.o"
+  "CMakeFiles/search_server.dir/remove_duplicates.cpp.o.d"
+  "CMakeFiles/search_server.dir/request_queue.cpp.o"
+  "CMakeFiles/search_server.dir/request_queue.cpp.o.d"
+  "CMakeFiles/search_server.dir/search_server.cpp.o"
+  "CMakeFiles/search_server.dir/search_server.cpp.o.d"
+  "CMakeFiles/search_server.dir/string_processing.cpp.o"
+  "CMakeFiles/search_server.dir/string_processing.cpp.o.d"
+  "search_server"
+  "search_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/search_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sprint8/Параллельные_алгоритмы/Параллелим_методы_поисковой_системы/Task/main.cpp
@@ -0,0 +1,92 @@
+#include "process_queries.h"
+#include "search_server.h"
+
+#include "search_server.h"
+
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+#include "log_duration.h"
+
+using namespace std;
+
+string GenerateWord(mt19937& generator, int max_length) {
+    const int length = uniform_int_distribution(1, max_length)(generator);
+    string word;
+    word.reserve(length);
+    for (int i = 0; i < length; ++i) {
+        word.push_back(uniform_int_distribution('a', 'z')(generator));
+    }
+    return word;
+}
+
+vector<string> GenerateDictionary(mt19937& generator, int word_count, int max_length) {
+    vector<string> words;
+    words.reserve(word_count);
+    for (int i = 0; i < word_count; ++i) {
+        words.push_back(GenerateWord(generator, max_length));
+    }
+    sort(words.begin(), words.end());
+    words.erase(unique(words.begin(), words.end()), words.end());
+    return words;
+}
+
+string GenerateQuery(mt19937& generator, const vector<string>& dictionary, int max_word_count) {
+    const int word_count = uniform_int_distribution(1, max_word_count)(generator);
+    string query;
+    for (int i = 0; i < word_count; ++i) {
+        if (!query.empty()) {
+            query.push_back(' ');
+        }
+        query += dictionary[uniform_int_distribution<int>(0, dictionary.size() - 1)(generator)];
+    }
+    return query;
+}
+
+vector<string> GenerateQueries(mt19937& generator, const vector<string>& dictionary, int query_count, int max_word_count) {
+    vector<string> queries;
+    queries.reserve(query_count);
+    for (int i = 0; i < query_count; ++i) {
+        queries.push_back(GenerateQuery(generator, dictionary, max_word_count));
+    }
+    return queries;
+}
+
+template <typename ExecutionPolicy>
+void Test(string_view mark, SearchServer search_server, ExecutionPolicy&& policy) {
+    LOG_DURATION(mark);
+    const int document_count = search_server.GetDocumentCount();
+    cout << search_server.GetDocumentCount() << " ";
+    for (int id = 0; id < document_count; ++id) {
+        search_server.RemoveDocument(policy, id);
+    }
+    cout << search_server.GetDocumentCount() << endl;
+}
+
+#define TEST(mode) Test(#mode, search_server, execution::mode)
+
+int main() {
+    mt19937 generator;
+
+    const auto dictionary = GenerateDictionary(generator, 10'000, 25);
+    const auto documents = GenerateQueries(generator, dictionary, 10'000, 100);
+
+    {
+        SearchServer search_server(dictionary[0]);
+        for (size_t i = 0; i < documents.size(); ++i) {
+            search_server.AddDocument(i, documents[i], DocumentStatus::ACTUAL, {1, 2, 3});
+        }
+
+        TEST(seq);
+    }
+    {
+        SearchServer search_server(dictionary[0]);
+        for (size_t i = 0; i < documents.size(); ++i) {
+            search_server.AddDocument(i, documents[i], DocumentStatus::ACTUAL, {1, 2, 3});
+        }
+
+        TEST(par);
+    }
+}
