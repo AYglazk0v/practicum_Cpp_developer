514dc0e8d91e7c830cf6ee88d6ee9b49c6614fb0
Goblin Tagganaro <AYGlazkov@stud.kpfu.ru>
2023-06-06 13:40:47 +0300

    Solved 09-01-02-00&&01

---
+++ sprint9/Пространства_имён/Имена_и_пространства_имён/Task1/main.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+// не меняйте файлы json.h и json.cpp
+#include "json.h"
+
+using namespace std;
+
+struct Spending {
+    string category;
+    int amount;
+};
+
+int CalculateTotalSpendings(const vector<Spending>& spendings) {
+    return accumulate(
+        spendings.begin(), spendings.end(), 0, [](int current, const Spending& spending){
+            return current + spending.amount;
+        });
+}
+
+string FindMostExpensiveCategory(const vector<Spending>& spendings) {
+    assert(!spendings.empty());
+    auto compare_by_amount = [](const Spending& lhs, const Spending& rhs) {
+        return lhs.amount < rhs.amount;
+    };
+    return max_element(begin(spendings), end(spendings), compare_by_amount)->category;
+}
+
+vector<Spending> LoadFromJson(istream& input) {
+    vector<Spending> ret_vector;
+    auto doc = Load(input);
+    for (auto& i : doc.GetRoot().AsArray()) {
+        auto& map_ = i.AsMap();
+        ret_vector.emplace_back(std::move(map_.at("category").AsString()), map_.at("amount").AsInt());
+    }
+    return ret_vector;
+}
+
+int main() {
+    // не меняйте main
+    const vector<Spending> spendings = LoadFromJson(cin);
+    cout << "Total "sv << CalculateTotalSpendings(spendings) << '\n';
+    cout << "Most expensive is "sv << FindMostExpensiveCategory(spendings) << '\n';
+}
---
+++ sprint9/Пространства_имён/Имена_и_пространства_имён/Task2/main.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+// не меняйте файлы xml.h и xml.cpp
+#include "xml.h"
+
+using namespace std;
+
+struct Spending {
+    string category;
+    int amount;
+};
+
+int CalculateTotalSpendings(const vector<Spending>& spendings) {
+    return accumulate(spendings.begin(), spendings.end(), 0,
+                      [](int current, const Spending& spending) {
+                          return current + spending.amount;
+                      });
+}
+
+string FindMostExpensiveCategory(const vector<Spending>& spendings) {
+    assert(!spendings.empty());
+    auto compare_by_amount = [](const Spending& lhs, const Spending& rhs) {
+        return lhs.amount < rhs.amount;
+    };
+    return max_element(begin(spendings), end(spendings), compare_by_amount)->category;
+}
+
+vector<Spending> LoadFromXml(istream& input) {
+    vector<Spending> ret_vector;
+    auto doc = Load(input);
+    for (auto& i : doc.GetRoot().Children()) {
+        ret_vector.emplace_back(std::move(i.AttributeValue<std::string>("category")), i.AttributeValue<int>("amount"));
+    }
+    return ret_vector;
+}
+
+int main() {
+    const vector<Spending> spendings = LoadFromXml(cin);
+    cout << "Total "sv << CalculateTotalSpendings(spendings) << '\n';
+    cout << "Most expensive is "sv << FindMostExpensiveCategory(spendings) << '\n';
+}
